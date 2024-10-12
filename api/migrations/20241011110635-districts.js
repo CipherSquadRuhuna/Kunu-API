9c2896134d62019a92c7016a24a17aafeeafd8ed
Asela <43272744+zaselalk@users.noreply.github.com>
2024-10-12 16:16:54 +0530

    Refactor ComplainController.js and related files
    - Renamed createComplain function to createGarbageComplain
    - Added createServiceComplain function to handle service complains
    - Created migration and seeder files for service complains table
    - Updated ComplainRoutes.js to handle garbage and service complains separately
    - Updated createComplainSchema.js to include validation for service complains

--- api/migrations/20241011110635-districts.js
+++ api/migrations/20241011110635-districts.js
@@ -4,7 +4,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable("districts", {
-      district_id: {
+      id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
         autoIncrement: true,
