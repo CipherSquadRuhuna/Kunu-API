603bab9bfb6ef0208ce7a1ed88114b2f4d96abe4
Asela <43272744+zaselalk@users.noreply.github.com>
2024-10-14 08:58:15 +0530

    Refactor webhook message endpoint and add webhook subscription endpoint

--- api/app.js
+++ api/app.js
@@ -20,14 +20,44 @@ app.use("/api/v1/data", DataRoutes);
 app.use("/api/v1/schedule", require("./routes/ScheduleRoutes.js"));
 app.use("/api/v1/request-pickup", require("./routes/RequestPickupRoutes.js"));
 // app.use("/api/v1/payment", require("./routes/PaymentRoutes.js"));
-app.use("/api/webhook/message", (req, res) => {
-  console.log(req.body);
-  res.json({ message: "ok" });
+// app.use("/api/webhook/message", (req, res) => {
+//   console.log(req.body);
+//   res.json({ message: "ok" });
+// });
+
+app.use("/api/webhook/message/send", (req, res) => {
+  var axios = require("axios");
+  var data = JSON.stringify({
+    applicationId: "APP_066293",
+    password: "4c49aba32fb8a656f02f8fbf2d443ba9",
+    subscriberId: "tel:94755383869",
+    version: "1.0",
+    action: "1",
+  });
+
+  var config = {
+    method: "post",
+    url: "https://api.dialog.lk/subscription/send",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: data,
+  };
+
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 });
+
 app.use("/api/v1/webhook/subscription", (req, res) => {
   console.log(req.body);
   res.json({ message: "ok" });
 });
+
 app.use(ErrorHandleMiddleware);
 
 module.exports = { app };
