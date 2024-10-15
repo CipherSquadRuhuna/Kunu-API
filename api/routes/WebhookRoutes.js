d01e068b637fca39716061dc6b624162693a951a
Asela <43272744+zaselalk@users.noreply.github.com>
2024-10-15 14:14:13 +0530

    Refactor WebhookRoutes.js, update subscriberId and action values

--- api/routes/WebhookRoutes.js
+++ api/routes/WebhookRoutes.js
@@ -5,9 +5,9 @@ routes.post("/subscription", (req, res) => {
   var data = JSON.stringify({
     applicationId: "APP_066293",
     password: "4c49aba32fb8a656f02f8fbf2d443ba9",
-    subscriberId: "tel:94755383869",
     version: "1.0",
-    action: "0",
+    action: "1",
+    subscriberId: "tel:94771242254",
   });
 
   var config = {
