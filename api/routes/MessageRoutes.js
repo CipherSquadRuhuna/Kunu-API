fb2591bedbd4ad844b67703d4775ec6bb9de7e86
Asela <43272744+zaselalk@users.noreply.github.com>
2024-10-15 18:54:28 +0530

    Refactor MessageRoutes.js, update send route to include message and subscriberId

--- api/routes/MessageRoutes.js
+++ api/routes/MessageRoutes.js
@@ -3,11 +3,11 @@ const axios = require("axios");
 
 router.post("/send", (req, res) => {
   const data = JSON.stringify({
+    message: "Hello",
     applicationId: "APP_066293",
     password: "4c49aba32fb8a656f02f8fbf2d443ba9",
-    message: "Hello",
     destinationAddresses: [
-      "tel:B%3C4clUUo8U11COKJXFBh8O5TBIPLKZcmjd+Mzr+7QqxuL215qcEk/S0I3zrteFr4+hD",
+      "B%3C4clUUo8U11COKJXFBh8O5TBIPLKZcmjd+Mzr+7QqxuL215qcEk/S0I3zrteFr4+hD",
     ],
   });
 
