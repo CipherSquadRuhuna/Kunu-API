a41c67be4eb28fe58c8d5cb721040eaf8a6287fc
Asela <43272744+zaselalk@users.noreply.github.com>
2024-10-15 18:48:09 +0530

    Refactor MessageRoutes.js to include destinationAddresses in send route

--- api/routes/MessageRoutes.js
+++ api/routes/MessageRoutes.js
@@ -6,8 +6,9 @@ router.post("/send", (req, res) => {
     applicationId: "APP_066293",
     password: "4c49aba32fb8a656f02f8fbf2d443ba9",
     message: "Hello",
-    subscriberId:
+    destinationAddresses: [
       "tel:B%3C4clUUo8U11COKJXFBh8O5TBIPLKZcmjd+Mzr+7QqxuL215qcEk/S0I3zrteFr4+hD",
+    ],
   });
 
   const config = {
